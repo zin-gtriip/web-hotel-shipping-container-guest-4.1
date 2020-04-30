1940c33d27f1c9b70dc1fd938f6c97e1ee80b09c
Oktavianus Chua <oktavianus@gtriipglobal.com>
2020-04-30 17:59:23 +0700

    add sample data

---
+++ guest_app/samples.py
@@ -0,0 +1,23 @@
+checkin_data = [
+    {
+        'confirmation_number': '1234',
+        'arrival_date': '2020-04-01',
+        'last_name': 'TEST',
+    },
+    {
+        'confirmation_number': '12345',
+        'arrival_date': '2020-04-01',
+        'last_name': 'TEST',
+    }
+]
+
+def get_data(post_data):
+    sample_data = next((data for data in checkin_data \
+        if data['confirmation_number'] == post_data['confirmation_number'] and \
+        data['arrival_date'] == str(post_data['arrival_date']) and \
+        data['last_name'] == str(post_data['last_name'])), {})
+    if sample_data:
+        sample_data['status'] = 'success'
+    else:
+        sample_data = {'status': 'error', 'message': 'Error connecting to server'}
+    return sample_data
