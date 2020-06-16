3d99a92bb4fde154d2f23b0b6ebd776b96a19669
Oktavianus Chua <oktavianus@gtriipglobal.com>
2020-06-16 16:38:18 +0700

    login and passport view: change logic when set session data based on api response

--- guest_app/forms/check_in.py
+++ guest_app/forms/check_in.py
@@ -2,11 +2,8 @@ import os
 import base64
 from django                     import forms
 from django.utils.translation   import gettext, gettext_lazy as _
-from django.core.cache          import caches
 from django.conf                import settings
 from guest_app                  import gateways, utilities, samples
-
-CACHE = caches['default']
 
 class CheckInLoginForm(forms.Form):
     reservation_no  = forms.CharField(label=_('Reservation Number'))
@@ -31,26 +28,24 @@ class CheckInLoginForm(forms.Form):
             self._errors['arrival_date'] = self.error_class([_('Enter the required information')])
         if not last_name:
             self._errors['last_name'] = self.error_class([_('Enter the required information')])
-        # validate from backend
-        if reservation_no and arrival_date and last_name:
-            data = {
-                'confirmation_number': reservation_no,
-                'arrival_date': arrival_date,
-                'last_name': last_name,
-            }
-            response = samples.get_data(data) #gateways.post('/booking/get_booking', data)
-            if response.get('status', '') == 'success':
-                CACHE.set('check_in_login', response) # save to cache to be used after form is valid
-            else:
-                raise forms.ValidationError(response.get('message', _('Unknown error')))
         return self.cleaned_data
 
-    def set_session(self):
+    def gateway_post(self):
+        data = {
+            'confirmation_number': self.cleaned_data.get('reservation_no'),
+            'arrival_date': self.cleaned_data.get('arrival_date'),
+            'last_name': self.cleaned_data.get('last_name'),
+        }
+        response = samples.get_data(data) #gateways.post('/booking/get_booking', data)
+        if response.get('status', '') != 'success':
+            self._errors[forms.forms.NON_FIELD_ERRORS] = self.error_class([response['message'] or _('Unknown error')])
+        return response
+    
+    def set_session(self, data):
         self.request.session.flush()
         self.request.session.create() # for creating `session_key`
         self.request.session.set_expiry(settings.CHECK_IN_SESSION_AGE)
-        self.request.session['check_in_data'] = CACHE.get('check_in_login', {})
-        CACHE.clear()
+        self.request.session['check_in_data'] = {'booking_details': data}
 
 
 class CheckInPassportForm(forms.Form):
@@ -67,37 +62,47 @@ class CheckInPassportForm(forms.Form):
         passport_file = self.cleaned_data.get('passport_file')
         skip_passport = self.cleaned_data.get('skip_passport')
 
-        if not skip_passport:
-            # validate passport_file not empty
-            if not passport_file:
-                raise forms.ValidationError(_('No image file selected.'))
-            
-            # save passport file using `session_key` as file name
-            file_name = self.request.session.session_key +'.png'
-            folder_name = os.path.join(settings.BASE_DIR, 'upload')
-            if not os.path.exists(folder_name):
-                os.mkdir(folder_name)
-            saved_file = os.path.join(folder_name, file_name)
-            file_data = base64.b64decode(passport_file)
-            with open(saved_file, 'wb') as f:
-                f.write(file_data)
+        if not skip_passport and not passport_file:
+            raise forms.ValidationError(_('No image file selected.'))
+        return self.cleaned_data
 
-            # send to ocr scanning
-            scan_type = 'passport'
-            response = {'status': 'success', 'expired': 'false', 'date_of_birth': '20/10/1990'} # gateways.ocr(saved_file, 'passport')
-            if response.get('status', '') != 'success':
-                scan_type = 'nric'
-                response = gateways.ocr(saved_file, 'nric')
+    def gateway_ocr(self):
+        if self.cleaned_data.get('skip_passport'):
+            return
 
-            # validation based on `scan_type` (`passport` / `nric`)
+        # save passport file using `session_key` as file name
+        file_name = self.request.session.session_key +'.png'
+        folder_name = os.path.join(settings.BASE_DIR, 'upload')
+        if not os.path.exists(folder_name):
+            os.mkdir(folder_name)
+        saved_file = os.path.join(folder_name, file_name)
+        file_data = base64.b64decode(self.cleaned_data.get('passport_file'))
+        with open(saved_file, 'wb') as f:
+            f.write(file_data)
+
+        # send to ocr scanning
+        scan_type = 'passport'
+        response = samples.ocr_data # gateways.ocr(saved_file, 'passport')
+        if 'status' in response or 'message' in response:
+            scan_type = 'nric'
+            response = gateways.ocr(saved_file, 'nric')
+
+        # validation based on `scan_type` (`passport` / `nric`)
+        if 'status' not in response and 'message' not in response:
             if scan_type == 'passport':
                 if response.get('expired', '') == 'false':
                     if utilities.calculate_age(response.get('date_of_birth', '')) <= settings.PASSPORT_AGE_LIMIT:
-                        raise forms.ValidationError(_('You must be at least %(age)s years of age to proceed with your registration.') % {'age': settings.PASSPORT_AGE_LIMIT})
+                        self._errors[forms.forms.NON_FIELD_ERRORS] = self.error_class([_('You must be at least %(age)s years of age to proceed with your registration.') % {'age': settings.PASSPORT_AGE_LIMIT}])
                 else:
-                    raise forms.ValidationError(_('Your passport has expired, please capture / upload a valid passport photo to proceed'))
+                    self._errors[forms.forms.NON_FIELD_ERRORS] = self.error_class([_('Your passport has expired, please capture / upload a valid passport photo to proceed')])
+        else:
+            self._errors[forms.forms.NON_FIELD_ERRORS] = self.error_class([response['message'] or _('Unknown error')])
+        return response
 
-        return self.cleaned_data
+    def set_session(self, data):
+        if data is not None:
+            self.request.session['check_in_data'].update({'passport_ocr': data})
+            self.request.session.save()
 
 
 class CheckInDetailForm(forms.Form):
--- guest_app/views.py
+++ guest_app/views.py
@@ -7,13 +7,17 @@ from .mixins                    import *
 class IndexView(RedirectView):
     pattern_name = 'guest_app:check-in-login'
 
+
 class CheckInLoginView(RequestInitializedMixin, FormView):
     template_name   = 'check_in/login.html'
     form_class      = CheckInLoginForm
     success_url     = '/check_in/passport'
 
     def form_valid(self, form):
-        form.set_session()
+        data = form.gateway_post()
+        if form.errors:
+            return self.form_invalid(form)
+        form.set_session(data)
         return super().form_valid(form)
 
 
@@ -21,6 +25,13 @@ class CheckInPassportView(RequestInitializedMixin, SessionDataRequiredMixin, For
     template_name   = 'check_in/passport.html'
     form_class      = CheckInPassportForm
     success_url     = '/check_in/detail'
+
+    def form_valid(self, form):
+        data = form.gateway_ocr()
+        if form.errors:
+            return self.form_invalid(form)
+        form.set_session(data)
+        return super().form_valid(form)
 
 
 class CheckInDetailView(RequestInitializedMixin, SessionDataRequiredMixin, FormView):
