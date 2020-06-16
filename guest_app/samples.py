0e59407abb18360d7649fc5885cb811bdc027c63
Oktavianus Chua <oktavianus@gtriipglobal.com>
2020-06-16 16:32:33 +0700

    sample data: add ocr data

--- guest_app/samples.py
+++ guest_app/samples.py
@@ -3,15 +3,24 @@ from django.utils.translation   import gettext, gettext_lazy as _
 check_in_data = [
     {
         'confirmation_number': '1234',
-        'arrival_date': '2020-05-01',
+        'arrival_date': '2020-06-01',
         'last_name': 'TEST',
     },
     {
         'confirmation_number': '12345',
-        'arrival_date': '2020-05-01',
+        'arrival_date': '2020-06-01',
         'last_name': 'TEST',
     }
 ]
+
+ocr_data = {
+    'expired': 'false',
+    'date_of_birth': '20/10/1990',
+    'first_name': 'TEST1',
+    'last_name': 'TEST',
+    'nationality': 'IDN',
+    'number': '12345'
+}
 
 def get_data(post_data):
     sample_data = next((data for data in check_in_data \
