b079b4326b83f11249d0e1f9400b0f5b058bf2ad
Oktavianus Chua <oktavianus@gtriipglobal.com>
2020-06-22 16:46:28 +0700

    check in login, mobile: to add error message using modal, add default value for arrival time

--- guest_app/static/js/mobile/check_in/login.js
+++ guest_app/static/js/mobile/check_in/login.js
@@ -1,21 +1,31 @@
 $('.datepicker').datepicker();
+$('#id_arrival_date').datepicker('update', new Date());
 
 
-$('input').change(function() {
+$('input:visible').change(function() {
 	var $this = $(this);
 	if ($this.val() != '') {
 		$('#btn-step-next').attr('disabled', false);
 	} else {
 		$('#btn-step-next').attr('disabled', true);
 	}
-})
+});
+$('input').change();
 
 
 $('#btn-step-next').click(function() {
 	var $currentStep = $('.input-step.active')
 		, $nextStep = $currentStep.next('.input-step');
 
-	$(this).attr('disabled', true);
+	$nextStep.find('input').change(function () {
+		var $this = $(this);
+		if ($this.val() != '') {
+			$('#btn-step-next').attr('disabled', false);
+		} else {
+			$('#btn-step-next').attr('disabled', true);
+		}
+	});
+	$('input').change();
 
 	// submit if no more nextStep
 	if ($nextStep.length == 0) {
@@ -32,4 +42,15 @@ $('#btn-step-next').click(function() {
 		}
 		, duration: 1000
 	}).removeClass('active');
+});
+
+
+$('.modal.error-modal').each(function() {
+	if (!$('body').hasClass('modal-open')) {
+		$(this).modal('show').addClass('shown');
+	}
+	// trigger next modal
+	$(this).on('hidden.bs.modal', function(e) {
+		$('.modal.error-modal:not(.shown)').first().modal('show').addClass('shown');
+	});
 });
