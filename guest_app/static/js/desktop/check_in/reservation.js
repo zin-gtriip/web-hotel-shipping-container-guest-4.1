a8b6a20679a40c38f6d77482ad3409aca0842897
Oktavianus Chua <oktavianus@gtriipglobal.com>
2020-07-03 13:49:44 +0700

    reservation: to add logic populate default selected value

--- guest_app/static/js/desktop/check_in/reservation.js
+++ guest_app/static/js/desktop/check_in/reservation.js
@@ -1,3 +1,6 @@
+var reservation = JSON.parse($('#reservation').text());
+
+// event when reservation thumbnail is clicked
 $('.reservation-container').click(function() {
     $(this).addClass('active')
         .find('input').prop('checked', true).change();
@@ -5,6 +8,15 @@ $('.reservation-container').click(function() {
 });
 
 
+// enable next button when one of reservation is selected
 $('input[name=reservation]').change(function() {
     $('#btn-step-next').attr('disabled', false);
 });
+
+
+// default selected value condition
+if (reservation == '') {
+    $('.reservation-container:first').click();
+} else {
+    $('input[name=reservation][value='+ reservation +']').parents('.reservation-container').click();
+}
