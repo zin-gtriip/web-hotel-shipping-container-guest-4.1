345dc5964b75ee291b28cb313b8b53f488319cc1
Oktavianus Chua <oktavianus@gtriipglobal.com>
2020-07-15 10:56:04 +0700

    samples: add post data when submitting pre-arrival data

--- guest_app/samples.py
+++ guest_app/samples.py
@@ -1,6 +1,6 @@
 from django.utils.translation   import gettext, gettext_lazy as _
 
-check_in_data = [
+check_in_get_data = [
     {
         'confirmation_number': '1234',
         'arrival_date': '2020-06-01',
@@ -20,7 +20,7 @@ check_in_data = [
                 'check_in_date': 'Thu, 12/06/20',
                 'check_out_date': 'Mon, 18/06/20',
             }
-        ]
+        ],
     },
     {
         'confirmation_number': '12345',
@@ -29,17 +29,8 @@ check_in_data = [
     }
 ]
 
-ocr_data = {
-    'expired': 'false',
-    'date_of_birth': '20/10/1990',
-    'first_name': 'TEST1',
-    'last_name': 'TEST',
-    'nationality': 'IDN',
-    'number': '12345'
-}
-
 def get_data(post_data):
-    sample_data = next((data for data in check_in_data \
+    sample_data = next((data for data in check_in_get_data \
         if data['confirmation_number'] == post_data['confirmation_number'] and \
         data['arrival_date'] == str(post_data['arrival_date']) and \
         data['last_name'] == str(post_data['last_name'])), {})
@@ -48,3 +39,25 @@ def get_data(post_data):
     else:
         sample_data = {'status': 'error', 'message': _('Error connecting to server')}
     return sample_data
+
+
+check_in_post_data = {
+    'status': 'success',
+    'reservations': [
+        {
+            'identifier': '1',
+            'room_type': 'Deluxe Room',
+            'check_in_date': 'Thu, 12/06/20',
+            'check_out_date': 'Mon, 18/06/20',
+        },
+        {
+            'identifier': '2',
+            'room_type': 'Premier Room',
+            'check_in_date': 'Thu, 12/06/20',
+            'check_out_date': 'Mon, 18/06/20',
+        }
+    ],
+}
+
+def send_data(post_data):
+    return check_in_post_data
