c2ec7aaa66bcf41f72a1f34955041d42d52fc078
Oktavianus Chua <oktavianus@gtriipglobal.com>
2020-07-16 17:15:46 +0700

    desktop login page: revamp layout

--- guest_app/static/js/desktop/check_in/login.js
+++ guest_app/static/js/desktop/check_in/login.js
@@ -1,1 +1,25 @@
 $('.datepicker').datepicker();
+$('#id_arrival_date:not(.is_bound)').datepicker('update', new Date());
+
+
+$('#id_reservation_no, #id_last_name, #id_arrival_date').keyup(function() {
+    enableDisableButton();
+});
+$('#id_reservation_no, #id_last_name').keyup();
+
+
+$('#id_arrival_date').datepicker().on('changeDate', function() {
+    enableDisableButton();
+});
+
+function enableDisableButton() {
+    $reservationNo = $('#id_reservation_no')
+        , $lastName = $('#id_last_name')
+        , $arrivalDate = $('#id_arrival_date');
+
+    if ($reservationNo.val() != '' && $lastName.val() != '' && $arrivalDate.val() != '') {
+		$('#btn-step-next').attr('disabled', false);
+	} else {
+		$('#btn-step-next').attr('disabled', true);
+    }
+}
