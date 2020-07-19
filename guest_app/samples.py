4563a7c293fbb79afd6ff7fe83c0a83f7b24b927
Oktavianus Chua <oktavianus@gtriipglobal.com>
2020-07-19 16:35:18 +0700

    samples: update sample data

--- guest_app/samples.py
+++ guest_app/samples.py
@@ -1,60 +1,168 @@
 from django.utils.translation   import gettext, gettext_lazy as _
 
-check_in_get_data = [
-    {
-        'reservation_no': '1234',
-        'arrival_date': '2020-06-01',
-        'last_name': 'TEST',
-        'first_name': 'TEST',
-        'passport_no': '12345',
-        'nationality': 'SG',
-        'birth_date': '1995-07-15',
-        'adult_number': '2',
-        'child_number': '1',
-        'email': 'test@test.com',
-        'reservations': [
-            {
-                'identifier': '1',
-                'room_type': 'Deluxe Room',
-                'check_in_date': 'Thu, 12/06/20',
-                'check_out_date': 'Mon, 18/06/20',
-            },
-            {
-                'identifier': '2',
-                'room_type': 'Premier Room',
-                'check_in_date': 'Thu, 12/06/20',
-                'check_out_date': 'Mon, 18/06/20',
-            }
-        ],
-    },
-    {
-        'reservation_no': '12345',
-        'arrival_date': '2020-06-01',
-        'last_name': 'TEST',
-    }
-]
+check_in_get_data = {
+    "overall_status": 500,
+    "data": [
+        {
+            "property": "Amara Sanctuary",
+            "reservationNo": "1234",
+            "otaNo": "123456",
+            "reservationStatus": "reserved",
+            "adults": "3",
+            "children": "5",
+            "arrivalDate": "2020-07-14",
+            "departureDate": "2020-07-20",
+            "eta": "10:00:00",
+            "guestsId": [1, 2],
+            "guestsList": [
+                {
+                    "guestID": "1",
+                    "salutation": "Mr.",
+                    "firstName": "Columbus",
+                    "lastName": "Leonard",
+                    "passportNo": "A12345X",
+                    "nationality": "US",
+                    "dob": "1980-12-31",
+                    "email": "columbus@a.com",
+                    "emailSubscription": "1",
+                    "isMainGuest": 1
+                },
+                {
+                    "guestID": "2",
+                    "salutation": "Ms.",
+                    "firstName": "Jacquelin",
+                    "lastName": "F.",
+                    "passportNo": "A34236X",
+                    "nationality": "US",
+                    "dob": "1986-07-05",
+                    "email": "jacquelin@a.com",
+                    "emailSubscription": "0",
+                    "isMainGuest": 0
+                }
+            ],
+            "roomNo": "736",
+            "roomType": "Deluxe Room",
+            "roomRate": "99.98999786376953",
+            "roomStatus": "Ready",
+            "paymentType": "Credit card",
+            "paymentToken": "@\u0026@HF!",
+            "billsId": [1],
+            "comments": "Very good service and friendly front desk!",
+        },
+        {
+            "property": "Amara Sanctuary",
+            "reservationNo": "3333",
+            "otaNo": "123456",
+            "reservationStatus": "reserved",
+            "adults": "3",
+            "children": "5",
+            "arrivalDate": "2020-07-14",
+            "departureDate": "2020-07-20",
+            "eta": "10:00:00",
+            "guestsId": [5, 6, 7],
+            "guestsList": [
+                {
+                    "guestID": "5",
+                    "salutation": "Mr.",
+                    "firstName": "John",
+                    "lastName": "Leonard",
+                    "passportNo": "A85667X",
+                    "nationality": "US",
+                    "dob": "2010-12-02",
+                    "email": "-",
+                    "emailSubscription": "0",
+                    "isMainGuest": 0
+                },
+                {
+                    "guestID": "6",
+                    "salutation": "Mr.",
+                    "firstName": "Alex",
+                    "lastName": "Leonard",
+                    "passportNo": "A74574X",
+                    "nationality": "US",
+                    "dob": "2000-07-04",
+                    "email": "alexleo@a.com",
+                    "emailSubscription": "0",
+                    "isMainGuest": 1
+                },
+                {
+                    "guestID": "7",
+                    "salutation": "Ms.",
+                    "firstName": "Jane",
+                    "lastName": "Leonard",
+                    "passportNo": "A56345X",
+                    "nationality": "US",
+                    "dob": "2002-03-05",
+                    "email": "janeleo@a.com",
+                    "emailSubscription": "0",
+                    "isMainGuest": 0
+                }
+            ],
+            "roomNo": "737",
+            "roomType": "Premier Room",
+            "roomRate": "99.98999786376953",
+            "roomStatus": "Ready",
+            "paymentType": "Credit card",
+            "paymentToken": "@\u0026@HF!",
+            "billsId": [3],
+            "comments": "Very good service and friendly front desk!"
+        },
+    ]
+}
 
 def get_data(post_data):
-    sample_data = next((data for data in check_in_get_data \
-        if data['reservation_no'] == post_data['reservation_no'] and \
-        data['arrival_date'] == str(post_data['arrival_date']) and \
-        data['last_name'] == str(post_data['last_name'])), {})
-    if sample_data:
-        sample_data['status'] = 'success'
-    else:
-        sample_data = {'status': 'error', 'message': _('Error connecting to server')}
-    return sample_data
+    return check_in_get_data
 
 
 check_in_post_data = {
     'status': 'success',
-    'reservations': [
+    'data': [
         {
-            'identifier': '2',
-            'room_type': 'Premier Room',
-            'check_in_date': 'Thu, 12/06/20',
-            'check_out_date': 'Mon, 18/06/20',
-        }
+            "property": "Amara Sanctuary",
+            "reservationNo": "1234",
+            "otaNo": "123456",
+            "reservationStatus": "reserved",
+            "adults": "3",
+            "children": "5",
+            "arrivalDate": "2020-07-14",
+            "departureDate": "2020-07-20",
+            "eta": "10:00:00",
+            "guestsId": [1, 2],
+            "guestsList": [
+                {
+                    "guestID": "1",
+                    "salutation": "Mr.",
+                    "firstName": "Columbus",
+                    "lastName": "Leonard",
+                    "passportNo": "A12345X",
+                    "nationality": "US",
+                    "dob": "1980-12-31",
+                    "email": "columbus@a.com",
+                    "emailSubscription": "1",
+                    "isMainGuest": 1
+                },
+                {
+                    "guestID": "2",
+                    "salutation": "Ms.",
+                    "firstName": "Jacquelin",
+                    "lastName": "F.",
+                    "passportNo": "A34236X",
+                    "nationality": "US",
+                    "dob": "1986-07-05",
+                    "email": "jacquelin@a.com",
+                    "emailSubscription": "0",
+                    "isMainGuest": 0
+                }
+            ],
+            "roomNo": "736",
+            "roomType": "Deluxe Room",
+            "roomRate": "99.98999786376953",
+            "roomStatus": "Ready",
+            "paymentType": "Credit card",
+            "paymentToken": "@\u0026@HF!",
+            "billsId": [1],
+            "comments": "Very good service and friendly front desk!",
+        },
     ],
 }
 
