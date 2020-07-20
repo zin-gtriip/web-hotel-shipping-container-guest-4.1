2fc8f7e951ebe3788a461ea48ba4a5254be5631c
Oktavianus Chua <oktavianus@gtriipglobal.com>
2020-07-20 20:22:12 +0700

    utilities: change datetime variable name to "dt"

--- pre_arrival/utilities.py
+++ pre_arrival/utilities.py
@@ -1,5 +1,5 @@
-import datetime as dt
-from datetime       import datetime
+import datetime
+from datetime       import datetime as dt
 from django.utils   import timezone
 
 def calculate_age(date):
@@ -7,7 +7,7 @@ def calculate_age(date):
     Calculate age based on passed date and date now
     Passed date format needs to be `yyyy-mm-dd`
     """
-    if not date or not isinstance(date, dt.date): return 0
+    if not date or not isinstance(date, datetime.date): return 0
     today = timezone.now()
     return today.year - date.year - ((today.month, today.day) < (date.month, date.day))
 
@@ -17,7 +17,7 @@ def parse_ocr_date(date):
     Parse date from OCR response `dd-mm-yyyy` to date object
     """
     if not date: return
-    return datetime.strptime(date, '%d/%m/%Y')
+    return dt.strptime(date, '%d/%m/%Y')
 
 
 def format_display_date(date):
@@ -25,7 +25,7 @@ def format_display_date(date):
     Convert string to date format `Mon, 01/01/20`
     """
     try:
-        date = datetime.strptime(date, '%Y-%m-%d')
+        date = dt.strptime(date, '%Y-%m-%d')
     except:
         date = None
     if not date: return
@@ -37,11 +37,11 @@ def generate_arrival_time():
     Generate array of time with interval 30 sec
     """
     result = []
-    loop = datetime.strptime('00:00', '%H:%M')
-    end = datetime.strptime('23:59', '%H:%M')
+    loop = dt.strptime('00:00', '%H:%M')
+    end = dt.strptime('23:59', '%H:%M')
     while(loop < end):
-        result.append((datetime.strftime(loop, '%H:%M'), datetime.strftime(loop, '%H:%M')))
-        loop += dt.timedelta(minutes=30)
+        result.append((dt.strftime(loop, '%H:%M'), dt.strftime(loop, '%H:%M')))
+        loop += datetime.timedelta(minutes=30)
     return result
 
 
@@ -50,7 +50,7 @@ def parse_arrival_time(time):
     Convert string to time format `00:00`
     """
     try:
-        time = datetime.strptime(time, '%H:%M:%S')
+        time = dt.strptime(time, '%H:%M:%S')
     except:
         time = None
     if not time: return
