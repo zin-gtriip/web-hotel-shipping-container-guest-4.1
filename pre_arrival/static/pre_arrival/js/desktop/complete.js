4069ee4eeb964d9b0f69a0c26cbc293f79e802ea
Oktavianus Chua <oktavianus@gtriipglobal.com>
2020-08-25 18:02:23 +0700

    complete.js: fix "reservationNo.postMessage" bug

--- pre_arrival/static/pre_arrival/js/desktop/complete.js
+++ pre_arrival/static/pre_arrival/js/desktop/complete.js
@@ -1,4 +1,4 @@
 $('#btn-ok').click(function() {
-    var reservationNo = JSON.parse($('#reservation-no').text() || '""'); // set time we're counting down to
-    try { reservationNo.postMessage(reservationNo); } catch(error) {} // send message to app
+    var number = JSON.parse($('#reservation-no').text() || '""'); // set time we're counting down to
+    try { reservationNo.postMessage(number); } catch(error) {} // send message to app
 });
