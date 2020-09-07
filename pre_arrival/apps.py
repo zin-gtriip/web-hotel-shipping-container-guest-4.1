3e20111b7e9878989b55f69c05f4011b998948e9
Oktavianus Chua <oktavianus@gtriipglobal.com>
2020-09-07 16:52:41 +0700

    apps: change import mixins declaration

--- pre_arrival/apps.py
+++ pre_arrival/apps.py
@@ -1,7 +1,8 @@
-from django.apps    import AppConfig
-from guest_base     import mixins as GuestBaseMixins
+from django.apps        import AppConfig
+from guest_base.mixins  import DependentAppConfigMixin
 
-class PreArrivalConfig(GuestBaseMixins.DependentAppConfigMixin, AppConfig):
+
+class PreArrivalConfig(DependentAppConfigMixin, AppConfig):
     name            = 'pre_arrival'
     dependencies    = [
         'guest_base.apps.GuestBaseConfig',
