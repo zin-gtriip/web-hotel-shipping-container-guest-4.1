74551950f9437de4ceda10c81b1b238325ebf85a
Oktavianus Chua <oktavianus@gtriipglobal.com>
2020-09-10 11:07:14 +0700

    samples: update check out data

--- check_out/samples.py
+++ check_out/samples.py
@@ -1,50 +1,62 @@
 check_out_data = {
-    "overall_status": 500,
-    "data": [
+    'overall_status': 500,
+    'data': [
         {
-            'reservation_no': '1234',
-            'arrival_date': '2020-09-01',
-            'departure_date': '2020-09-10',
-            'bills': [
+            'reservation_no': '1111',
+            'first_name': 'Alex',
+            'last_name': 'Columbus',
+            'item_total_amount': 134.04,
+            'tax_amount': 13.43,
+            'grand_total': 147.47,
+            'status': 'checkout_enabled',
+            'items': [
                 {
-                    'first_name': 'TEST1',
-                    'last_name': 'TEST1',
-                    'room_no': '111',
-                    'bill_details': [
-                        {
-                            'date': '2020-09-02',
-                            'description': 'item description 1',
-                            'amount': 100.00,
-                        },
-                        {
-                            'date': '2020-09-03',
-                            'description': 'item description 2',
-                            'amount': 20.00,
-                        },
-                    ]
+                    'date': '2020-09-09',
+                    'item_name': 'food',
+                    'amount': 13.7,
+                    'ref_no': ''
                 },
                 {
-                    'first_name': 'TEST2',
-                    'last_name': 'TEST2',
-                    'room_no': '222',
-                    'bill_details': [
-                        {
-                            'date': '2020-09-04',
-                            'description': 'item description 3',
-                            'amount': 10.00,
-                        },
-                        {
-                            'date': '2020-09-05',
-                            'description': 'item description 4',
-                            'amount': 200.00,
-                        },
-                        {
-                            'date': '2020-09-06',
-                            'description': 'item description 5',
-                            'amount': 30.00,
-                        },
-                    ]
+                    'date': '2020-09-09',
+                    'item_name': 'food',
+                    'amount': 10.0
                 },
+                {
+                    'date': '2020-09-09',
+                    'item_name': 'special',
+                    'amount': 100.0
+                },
+                {
+                    'date': '2020-09-09',
+                    'item_name': 'special-special',
+                    'amount': 100.0
+                },
+                {
+                    'date': '2020-09-09',
+                    'item_name': 'food and drinks',
+                    'amount': 110.34
+                }
+            ]
+        },
+        {
+            "reservation_no": "3333",
+            "first_name": "Notalex",
+            "last_name": "Columbus",
+            "item_total_amount": 56.5,
+            "tax_amount": 5.7,
+            "grand_total": 62.2,
+            "status": ["checkout_enabled", "checkout_disabled"],
+            "items": [
+                {
+                    "date": "2020-09-09",
+                    "item_name": "food",
+                    "amount": 36.5
+                },
+                {
+                    "date": "2020-09-09",
+                    "item_name": "snacks",
+                    "amount": 20.0
+                }
             ]
         }
     ]
