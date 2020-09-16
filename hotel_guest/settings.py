f3d1a4d5dd72f51e859e73bec13558d86b61580b
Oktavianus Chua <oktavianus@gtriipglobal.com>
2020-09-16 10:13:18 +0700

    settings: add "django.humanize" to installed app, add gst, business no, and currency symbol

--- hotel_guest/settings.py
+++ hotel_guest/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'compressor',
     'widget_tweaks',
     'django_cron',
@@ -220,6 +221,9 @@ DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760 # 10 MB
 # General
 # page title on every page
 HOTEL_NAME          = 'GTRIIP'
+GST_NO              = 'A13572468B'
+BUSINESS_NO         = '975308642Z'
+CURRENCY_SYMBOL     = 'S$'
 # default django variable for date format
 DATE_INPUT_FORMATS  = ['%Y-%m-%d']
 # set session to be saved everytime
