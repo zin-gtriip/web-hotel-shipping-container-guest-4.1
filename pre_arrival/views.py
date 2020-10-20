65b6ed8cea99337dc24e9d4cd2a8f7ad950a3aff
Oktavianus Chua <oktavianus@gtriipglobal.com>
2020-10-20 21:20:28 +0700

    views: pre-arrival to change import sequence

--- pre_arrival/views.py
+++ pre_arrival/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts           import render
-from django.views.generic       import *
 from django.utils               import translation
 from django.utils.translation   import gettext, gettext_lazy as _
+from django.views.generic       import *
 from guest_base                 import views as GuestBaseViews
 from guest_base.mixins          import RequestFormKwargsMixin, MobileTemplateMixin, JSONResponseMixin
 from .forms                     import *
