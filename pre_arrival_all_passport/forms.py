deec503e3350d7dbc486e028be50c788b5afcd1b
Oktavianus Chua <oktavianus@gtriipglobal.com>
2020-10-21 14:25:46 +0700

    forms: pre-arrival all passport extra passport to fix error guest id is required, dob is unserializable

--- pre_arrival_all_passport/forms.py
+++ pre_arrival_all_passport/forms.py
@@ -62,12 +62,16 @@ class PreArrivalAllPassportExtraPassportForm(forms.Form):
         with open(saved_file, 'rb') as image_file:
             file_b64_encoded = base64.b64encode(image_file.read())
         ocr = self.gateway_ocr(saved_file)
+        dob = utilities.parse_ocr_date(ocr.get('date_of_birth', ''))
+        if dob:
+            date_format = settings.DATE_INPUT_FORMATS[0] if settings.DATE_INPUT_FORMATS else '%Y-%m-%d'
+            dob = dob.strftime(date_format)
         extra_guest = {}
-        extra_guest['guestID'] = ''
+        extra_guest['guestID'] = '0'
         extra_guest['firstName'] = ocr.get('names', '')
         extra_guest['lastName'] = ocr.get('surname', '')
         extra_guest['nationality'] = Country(ocr.get('nationality', '')).code
         extra_guest['passportNo'] = ocr.get('number', '')
-        extra_guest['dob'] = utilities.parse_ocr_date(ocr.get('date_of_birth', ''))
+        extra_guest['dob'] = dob
         extra_guest['passportImage'] = file_b64_encoded.decode()
         self.request.session['pre_arrival']['reservation']['guestsList'].append(extra_guest)
