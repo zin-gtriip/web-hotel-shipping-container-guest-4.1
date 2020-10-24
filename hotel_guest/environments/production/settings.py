d2a16e90405105e8e6cf4890337ae19cbc186eb0
Oktavianus Chua <oktavianus@gtriipglobal.com>
2020-10-24 18:25:54 +0700

    settings: add security configuration on qa and prod environment

--- hotel_guest/environments/production/settings.py
+++ hotel_guest/environments/production/settings.py
@@ -2,3 +2,15 @@ from hotel_guest.settings import *
 
 DEBUG = False
 WSGI_APPLICATION = 'hotel_guest.environments.production.wsgi.application'
+
+
+# Security
+# lib/python3.x/site-packages/django/conf/global_settings.py
+
+X_FRAME_OPTIONS             = 'DENY'
+SESSION_COOKIE_SECURE       = True
+SESSION_COOKIE_HTTPONLY     = True
+CSRF_COOKIE_SECURE          = True
+CSRF_COOKIE_HTTPONLY        = True
+SECURE_BROWSER_XSS_FILTER   = True
+SECURE_SSL_REDIRECT         = True
