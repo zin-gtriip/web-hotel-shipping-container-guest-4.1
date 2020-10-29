dcb639ceb73648c66b0ecfa6eccfddf2c9a121a6
Oktavianus Chua <oktavianus@gtriipglobal.com>
2020-10-29 15:16:32 +0700

    samples: check-out update sample data to new workflow

--- check_out/samples.py
+++ check_out/samples.py
@@ -1,96 +1,219 @@
-check_out_login = {
-    'status': 500,
+check_out_login_all = {
+    'status_code': 500,
     'data': [
         {
-            "reservation_no": "1111",
-            "first_name": "Columbus",
-            "last_name": "Leonard"
+            "reservation_no": "8125B",
+            "first_name": "YS",
+            "last_name": "Test"
         },
         {
-            "reservation_no": "3333",
-            "first_name": "Notcolumbus",
-            "last_name": "Leonard"
+            "reservation_no": "4FM1T",
+            "first_name": "Shae",
+            "last_name": "Test"
         }
     ]
 }
 
+check_out_login_1 = {
+    'status_code': 500,
+    'data': [
+        {
+            "reservation_no": "8125B",
+            "first_name": "YS",
+            "last_name": "Test"
+        }
+    ]
+}
 
-check_out_bill = {
-    "status": 500,
+check_out_login_2 = {
+    'status_code': 500,
+    'data': []
+}
+
+def get_login(**kwargs):
+    identifier = kwargs.get('id')
+    if identifier == 1:
+        return check_out_login_all
+    elif identifier == 2:
+        return check_out_login_1
+    else:
+        return check_out_login_2
+
+
+check_out_bill_all = {
+    "status_code": 500,
     "data": {
-        "item_total_amount": 134.03,
-        "tax_amount": 13.43,
-        "grand_total": 147.47,
-        "check_out_ok": 1,
-        "items": [
+        "reservation_info": [
             {
-                "date": "2020-07-14",
-                "item_name": "food",
-                "amount": 13.7
+                "reservation_no": "8125B",
+                "first_name": "YS",
+                "last_name": "Test",
+                "arrival_date": "2020-10-28",
+                "departure_date": "2020-10-29",
+                "room_no": "1010",
+                "room_type": "Premier",
+                "adults": 2,
+                "children": 0,
+                "check_out_status": 500,
+                "item_total_amount": 1099.5,
+                "tax_amount": 100.49,
+                "grand_total": 1199.99,
+                "items": [
+                    {
+                        "date": "2020-10-28",
+                        "item_name": "Item X",
+                        "amount": 600.0
+                    },
+                    {
+                        "date": "2020-10-28",
+                        "item_name": "Item Y",
+                        "amount": 400.0
+                    },
+                    {
+                        "date": "2020-10-28",
+                        "item_name": "Item Z",
+                        "amount": 99.5
+                    }
+                ]
             },
             {
-                "date": "2020-07-14",
-                "item_name": "food",
-                "amount": 10.0
-            },
-            {
-                "date": "2020-07-15",
-                "item_name": "special",
-                "amount": 100.0
-            },
-            {
-                "date": "2020-07-15",
-                "item_name": "special-special",
-                "amount": 100.0
-            },
-            {
-                "date": "2020-07-15",
-                "item_name": "food and drinks",
-                "amount": 110.34
+                "reservation_no": "4FM1T",
+                "first_name": "Shae",
+                "last_name": "Test",
+                "arrival_date": "2020-10-28",
+                "departure_date": "2020-10-29",
+                "room_no": "1010",
+                "room_type": "Premier",
+                "adults": 2,
+                "children": 0,
+                "check_out_status": 500,
+                "item_total_amount": 3099.5,
+                "tax_amount": 282.28,
+                "grand_total": 3382.78,
+                "items": [
+                    {
+                        "date": "2020-10-28",
+                        "item_name": "Test Item X",
+                        "amount": 600.0
+                    },
+                    {
+                        "date": "2020-10-28",
+                        "item_name": "Test Item Y",
+                        "amount": 4000.0
+                    },
+                    {
+                        "date": "2020-10-29",
+                        "item_name": "Test Item Z",
+                        "amount": 99.5
+                    }
+                ]
             }
-        ]
+        ],
+        "total_amount": 4582.77,
+        "overall_status": True
+    }
+}
+
+check_out_bill_1 = {
+    "status_code": 500,
+    "data": {
+        "reservation_info": [
+            {
+                "reservation_no": "8125B",
+                "first_name": "YS",
+                "last_name": "Test",
+                "arrival_date": "2020-10-28",
+                "departure_date": "2020-10-29",
+                "room_no": "1010",
+                "room_type": "Premier",
+                "adults": 2,
+                "children": 0,
+                "check_out_status": 500,
+                "item_total_amount": 1099.5,
+                "tax_amount": 100.49,
+                "grand_total": 1199.99,
+                "items": [
+                    {
+                        "date": "2020-10-28",
+                        "item_name": "Item X",
+                        "amount": 600.0
+                    },
+                    {
+                        "date": "2020-10-28",
+                        "item_name": "Item Y",
+                        "amount": 400.0
+                    },
+                    {
+                        "date": "2020-10-28",
+                        "item_name": "Item Z",
+                        "amount": 99.5
+                    }
+                ]
+            },
+        ],
+        "total_amount": 1199.99,
+        "overall_status": False
+    }
+}
+
+check_out_bill_2 = {
+    "status_code": 500,
+    "data": {
+        "reservation_info": [
+            {
+                "reservation_no": "4FM1T",
+                "first_name": "Shae",
+                "last_name": "Test",
+                "arrival_date": "2020-10-28",
+                "departure_date": "2020-10-29",
+                "room_no": "1010",
+                "room_type": "Premier",
+                "adults": 2,
+                "children": 0,
+                "check_out_status": 500,
+                "item_total_amount": 3099.5,
+                "tax_amount": 282.28,
+                "grand_total": 3382.78,
+                "items": [
+                    {
+                        "date": "2020-10-28",
+                        "item_name": "Test Item X",
+                        "amount": 600.0
+                    },
+                    {
+                        "date": "2020-10-28",
+                        "item_name": "Test Item Y",
+                        "amount": 4000.0
+                    },
+                    {
+                        "date": "2020-10-29",
+                        "item_name": "Test Item Z",
+                        "amount": 99.5
+                    }
+                ]
+            }
+        ],
+        "total_amount": 3382.78,
+        "overall_status": True
+    }
+}
+
+check_out_bill_3 = {
+    "status_code": 100,
+    "data": {
+        "reservation_info": [],
+        "total_amount": 0.0,
+        "overall_status": True
     }
 }
 
 def get_bill(**kwargs):
-    data = {}
-    reservation_no = kwargs.get('reservation_no', '')
-    if reservation_no == ['1111']:
-        data['reservation_info'] = [{
-            "reservation_no": "1111",
-            "first_name": "Columbus",
-            "last_name": "Leonard",
-            "room_no": "A123",
-            "arrival_date": "2020-09-01",
-            "departure_date": "2020-09-10",
-        }]
-    elif reservation_no == ['3333']:
-        data['reservation_info'] = [{
-            "reservation_no": "3333",
-            "first_name": "Notcolumbus",
-            "last_name": "Leonard",
-            "room_no": "B321", #B321
-            "arrival_date": "2020-09-02",
-            "departure_date": "2020-09-09",
-        }]
+    reservations_no = kwargs.get('reservation_no')
+    if len(reservations_no) > 1:
+        return check_out_bill_all
+    elif reservations_no == ['8125B']:
+        return check_out_bill_1
+    elif reservations_no == ['4FM1T']:
+        return check_out_bill_2
     else:
-        data['reservation_info'] = [
-            {
-                "reservation_no": "1111",
-                "first_name": "Columbus",
-                "last_name": "Leonard",
-                "room_no": "A123",
-                "arrival_date": "2020-09-01",
-                "departure_date": "2020-09-10",
-            },
-            {
-                "reservation_no": "3333",
-                "first_name": "Notcolumbus",
-                "last_name": "Leonard",
-                "room_no": "B321", #B321
-                "arrival_date": "2020-09-02",
-                "departure_date": "2020-09-09",
-            }
-        ]
-    check_out_bill['data'].update(**data)
-    return check_out_bill
+        return check_out_bill_3
