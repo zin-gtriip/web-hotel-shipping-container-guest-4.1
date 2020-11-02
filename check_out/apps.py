63d4eee708d8a7f54bf75cf6c6dd830023216a67
Oktavianus Chua <oktavianus@gtriipglobal.com>
2020-11-02 12:07:22 +0700

    apps: to have release note and app version

--- check_out/apps.py
+++ check_out/apps.py
@@ -4,6 +4,7 @@ from guest_base.mixins  import DependentAppConfigMixin
 
 class CheckOutConfig(DependentAppConfigMixin, AppConfig):
     name            = 'check_out'
+    version         = '1.0.0'
     dependencies    = [
         'guest_base.apps.GuestBaseConfig',
     ]
