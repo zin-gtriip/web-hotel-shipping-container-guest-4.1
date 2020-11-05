475b54127117a2629d803f7a809ba9b28fdaf49f
Oktavianus Chua <oktavianus@gtriipglobal.com>
2020-11-05 10:34:36 +0700

    release, apps: up version

--- check_out/apps.py
+++ check_out/apps.py
@@ -4,7 +4,7 @@ from guest_base.mixins  import DependentAppConfigMixin
 
 class CheckOutConfig(DependentAppConfigMixin, AppConfig):
     name            = 'check_out'
-    version         = '1.0.4'
+    version         = '1.0.5'
     dependencies    = [
         'guest_base.apps.GuestBaseConfig',
     ]
