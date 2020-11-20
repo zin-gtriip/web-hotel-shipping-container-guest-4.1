31833081da2b9a91883130bf5870ec791a31e6ab
Phu Wai Paing <phuwai@gtriipglobal.com>
2020-11-20 17:45:29 +0800

    modify stable button size and change text to capitalize from ocr

--- check_out/views.py
+++ check_out/views.py
@@ -69,6 +69,7 @@ class CheckOutBillView(BillRequiredAndExistMixin, RequestFormKwargsMixin, Update
     def gateway_post(self, reservations_no):
         data = {'reservation_no': reservations_no}
         response = gateways.guest_endpoint('/billsForCheckOut', data)
+        print(response)
         return response.get('data', {})
 
     def get_object(self, queryset=None):
