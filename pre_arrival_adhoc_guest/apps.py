a5ed1e93135e3acdfab0d9ed6222fb6610dfa831
Oktavianus Chua <oktavianus@gtriipglobal.com>
2021-01-18 18:00:37 +0700

    release, apps: up version

--- pre_arrival_adhoc_guest/apps.py
+++ pre_arrival_adhoc_guest/apps.py
@@ -4,7 +4,7 @@ from guest_base.mixins  import DependentAppConfigMixin
 
 class PreArrivalAdhocGuestConfig(AppConfig):
     name            = 'pre_arrival_adhoc_guest'
-    version         = '1.3.1'
+    version         = '1.3.2'
     dependencies    = [
         'pre_arrival.apps.PreArrivalConfig',
         'pre_arrival_all_passport.apps.PreArrivalAllPassportConfig',
