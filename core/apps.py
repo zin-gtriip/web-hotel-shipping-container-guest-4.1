82d2fecabeaa8f85f7c6d25bcc2434b3d2aa26ef
Oktavianus Chua <oktavianus@gtriipglobal.com>
2021-01-25 11:52:44 +0700

    apps: rename app "guest_base" to "core"

---
+++ core/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class CoreConfig(AppConfig):
+    name    = 'core'
+    version = '1.3.1'
--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.urls    import path
 from .views         import *
 
-app_name = 'guest_base'
+app_name = 'core'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
 ]
