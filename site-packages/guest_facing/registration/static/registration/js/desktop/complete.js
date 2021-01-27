dd1d19a810bd67e8f8906657c8bfc1ac092047d3
Oktavianus Chua <oktavianus@gtriipglobal.com>
2021-01-27 17:04:59 +0700

    lib: to remove folder name to "site-packages"

--- site-packages/guest_facing/registration/static/registration/js/desktop/complete.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("flutterInAppWebViewPlatformReady", null); // add event to send message to app
-
-
-$('#btn-ok').click(function() {
-    var number = JSON.parse($('#reservation-no').text() || '""');
-    try { window.flutter_inappwebview.callHandler('reservationNo', number); } catch(error) {} // send message to app
-});
