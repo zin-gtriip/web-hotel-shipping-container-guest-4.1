aa13b69ffe407703dfee32170166002322fd1409
Oktavianus Chua <oktavianus@gtriipglobal.com>
2021-01-28 16:38:34 +0700

    mixins: core to update error message on "DependentAppConfigMixin"

--- lib/guest_facing/core/mixins.py
+++ lib/guest_facing/core/mixins.py
@@ -10,7 +10,7 @@ class DependentAppConfigMixin:
 
     def ready(self):
         if not all(depend in settings.INSTALLED_APPS for depend in self.dependencies):
-            raise ValueError('"%s" app depends on %s' % (self.name, ', '.join(self.dependencies)))
+            raise ValueError('"%s" app depends on "%s"' % (self.name, ', '.join(self.dependencies)))
         return super().ready()
 
 
