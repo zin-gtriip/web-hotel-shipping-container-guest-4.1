e7dc7a94c025bfe4adcbb32f333477d4f31ffc50
Oktavianus Chua <oktavianus@gtriipglobal.com>
2021-01-29 12:15:30 +0700

    gateways: add logging function on every function

--- guest_base/gateways.py
+++ guest_base/gateways.py
@@ -1,8 +1,12 @@
 import requests
 import json
+import logging
+from datetime                   import datetime as dt
 from json                       import JSONDecodeError
 from django.conf                import settings
 from django.utils.translation   import gettext, gettext_lazy as _
+
+logger = logging.getLogger('gateways')
 
 
 # guest facing endpoint gateway
@@ -12,6 +16,7 @@ def guest_endpoint(url, post_data):
     post_data['site_id'] = settings.GUEST_ENDPOINT_SITE_ID
     post_data['site_name'] = settings.GUEST_ENDPOINT_SITE_NAME
     try:
+        logger.info(str(dt.now().strftime('[%Y-%m-%d %H:%M:%S.%f]')) + ': REQUEST ' + str(url) + ' ' + str(post_data))
         response = requests.post(url, json=post_data, timeout=settings.GUEST_ENDPOINT_TIMEOUT_LIMIT, verify=False)
         response.raise_for_status()
         json_response = json.loads(response.content.decode('utf-8'))
@@ -22,6 +27,7 @@ def guest_endpoint(url, post_data):
             json_response = {'status': 'error', 'message': response.content.decode('utf-8')}
     except requests.exceptions.RequestException as request_error:
         json_response = {'status': 'error', 'message': _('Unable to connect to server, please try again.')}
+    logger.info(str(dt.now().strftime('[%Y-%m-%d %H:%M:%S.%f]')) + ': RESPONSE ' + str(json_response))
     return json_response
 
 
@@ -33,6 +39,7 @@ def amp_endpoint(url):
     post_data['site_id'] = settings.AMP_ENDPOINT_SITE_ID
     post_data['site_name'] = settings.AMP_ENDPOINT_SITE_NAME
     try:
+        logger.info(str(dt.now().strftime('[%Y-%m-%d %H:%M:%S.%f]')) + ': REQUEST ' + str(url) + ' ' + str(post_data))
         response = requests.post(url, json=post_data, timeout=settings.AMP_ENDPOINT_TIMEOUT_LIMIT, verify=False)
         response.raise_for_status()
         json_response = json.loads(response.content.decode('utf-8'))
@@ -43,6 +50,7 @@ def amp_endpoint(url):
             json_response = {'status': 'error', 'message': response.content.decode('utf-8')}
     except requests.exceptions.RequestException as request_error:
         json_response = {'status': 'error', 'message': _('Unable to connect to server, please try again.')}
+    logger.info(str(dt.now().strftime('[%Y-%m-%d %H:%M:%S.%f]')) + ': RESPONSE ' + str(json_response))
     return json_response
 
 
@@ -52,6 +60,7 @@ def ocr(image_file, scan_type):
     files = {scan_type: open(image_file, 'rb')}
     data = {'api_key': settings.OCR_ENDPOINT_KEY}
     try:
+        logger.info(str(dt.now().strftime('[%Y-%m-%d %H:%M:%S.%f]')) + ': REQUEST ' + str(url))
         response = requests.post(url, files=files, data=data, timeout=settings.OCR_ENDPOINT_TIMEOUT_LIMIT)
         response.raise_for_status()
         json_response = json.loads(response.content.decode('utf-8'))
@@ -62,4 +71,5 @@ def ocr(image_file, scan_type):
             json_response = {'status': 'error', 'message': response.content.decode('utf-8')}
     except requests.exceptions.RequestException as request_error:
         json_response = {'status': 'error', 'message': _('Error connecting to server')}
+    logger.info(str(dt.now().strftime('[%Y-%m-%d %H:%M:%S.%f]')) + ': RESPONSE ' + str(json_response))
     return json_response
