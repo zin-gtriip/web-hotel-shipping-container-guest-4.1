8b77a9ab338bf4c659bd7a60029e782905dc3fd6
Oktavianus Chua <oktavianus@gtriipglobal.com>
2021-03-09 11:16:02 +0700

    templates: pre-arrival to fix when timer is expired redirect to login instead of pre-arrival home

--- pre_arrival/static/pre_arrival/js/base.js
+++ pre_arrival/static/pre_arrival/js/base.js
@@ -81,7 +81,7 @@ $('#form-timer-extension').submit(function() {
 
 // timer expired
 $('#timer-expired-modal').on('hidden.bs.modal', function (e) {
-    window.location.href = '/';
+    window.location.href = '/pre_arrival/login/';
 });
 
 
