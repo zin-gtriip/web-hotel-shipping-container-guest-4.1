9746d2a34fd1f29fe472fe82bf3c763e648d8263
Oktavianus Chua <oktavianus@gtriipglobal.com>
2021-03-25 17:34:56 +0700

    release, apps: up version

--- pre_arrival/apps.py
+++ pre_arrival/apps.py
@@ -4,7 +4,7 @@ from guest_base.mixins  import DependentAppConfigMixin
 
 class PreArrivalConfig(DependentAppConfigMixin, AppConfig):
     name            = 'pre_arrival'
-    version         = '1.3.8'
+    version         = '1.4.2'
     dependencies    = [
         'guest_base.apps.GuestBaseConfig',
     ]
