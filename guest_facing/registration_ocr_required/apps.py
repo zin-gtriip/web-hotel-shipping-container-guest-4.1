945fc041cea894b3bc3581ca32941f7ef24bccc8
Oktavianus Chua <oktavianus@gtriipglobal.com>
2021-04-08 16:10:06 +0700

    apps: remove app for ocr required, adhoc guest

--- guest_facing/registration_ocr_required/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps                import AppConfig
-from guest_facing.core.mixins   import DependentAppConfigMixin
-
-
-class RegistrationOcrRequired(DependentAppConfigMixin, AppConfig):
-    name            = 'guest_facing.registration_ocr_required'
-    dependencies    = [
-        'guest_facing.registration',
-    ]
