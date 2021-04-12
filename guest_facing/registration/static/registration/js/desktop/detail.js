bc3c1a0a6e0fffbc43678b876dfcceda315455a3
Oktavianus Chua <oktavianus@gtriipglobal.com>
2021-04-12 16:07:52 +0700

    templates: registration detail to change layout modal confirmation

--- guest_facing/registration/static/registration/js/desktop/detail.js
+++ guest_facing/registration/static/registration/js/desktop/detail.js
@@ -15,16 +15,16 @@ $('.btn-passport').click(function() {
 
 $('#btn-back').click(function() {
 	var modal =
-		'<div class="modal fade" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-hidden="true">' +
+		'<div class="modal fade" id="modal-confirmation" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-hidden="true">' +
             '<div class="modal-dialog modal-dialog-centered modal-sm">'+
                 '<div class="modal-content">'+
                     '<div class="modal-header bg-primary text-white">'+
                         '<h4 class="modal-title mx-auto" id="modal-label-{{ forloop.counter0 }}">'+ gettext('Warning') +'</h4>'+
                     '</div>'+
                     '<div class="modal-body">'+ gettext('All changes is not saved yet. Are you sure want to do this?') +'</div>'+
-                    '<div class="modal-footer">'+
-                        '<a href="/registration/guest_list/" class="btn btn-outline-primary mx-auto">'+ gettext('Yes') +'</a>'+
-                        '<button type="button" class="btn btn-primary mx-auto" data-dismiss="modal">'+ gettext('No') +'</button>'+
+                    '<div class="modal-footer justify-content-center">'+
+                        '<a href="/registration/guest_list/" class="btn btn-outline-primary">'+ gettext('Yes') +'</a>'+
+                        '<button type="button" class="btn btn-primary" data-dismiss="modal">'+ gettext('No') +'</button>'+
                     '</div>'+
                 '</div>'+
             '</div>'+
