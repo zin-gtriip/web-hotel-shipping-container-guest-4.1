226b701444ebdea805218f014fa5877e78bbe2d9
Oktavianus Chua <oktavianus@gtriipglobal.com>
2021-05-25 10:39:33 +0700

    forms: registration detail to update ocr field changes

--- guest_facing/registration/forms.py
+++ guest_facing/registration/forms.py
@@ -193,13 +193,10 @@ class RegistrationDetailForm(forms.Form):
             birth_date = self.request.session.get('registration', {}).get('preload', {}).get('birth_date', birth_date)
         # from `ocr`
         if self.instance.get('is_overwrite', False):
-            first_name = self.request.session.get('registration', {}).get('ocr', {}).get('names', first_name)
+            first_name = self.request.session.get('registration', {}).get('ocr', {}).get('first_name', first_name)
             if self.instance.get('guestId', 0) == 0:
-                last_name = self.request.session.get('registration', {}).get('ocr', {}).get('surname', last_name)
-            if self.request.session.get('registration', {}).get('ocr', {}).get('document_type') != 'passport':
-                passport_no = self.request.session.get('registration', {}).get('ocr', {}).get('id_number', passport_no)
-            else:
-                passport_no = self.request.session.get('registration', {}).get('ocr', {}).get('number', passport_no)
+                last_name = self.request.session.get('registration', {}).get('ocr', {}).get('last_name', last_name)
+            passport_no = self.request.session.get('registration', {}).get('ocr', {}).get('number', passport_no)
             nationality = Country(self.request.session.get('registration', {}).get('ocr', {}).get('nationality', '')).code or nationality
             birth_date = self.request.session.get('registration', {}).get('ocr', {}).get('date_of_birth', birth_date)
         # assign
