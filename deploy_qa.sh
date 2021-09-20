1ccc7533ec7c0c25853e424842ead5a536248998
Phu Wai Paing <phuwai@gtriipglobal.com>
2021-09-20 12:14:39 +0800

    modify bash file

--- deploy_qa.sh
+++ deploy_qa.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ssh -tt -oStrictHostKeyChecking=no -i /home/phuwai/.ssh/id_rsa phu@54.179.10.115 <<EOF
+ssh -tt -oStrictHostKeyChecking=no phu@54.179.10.115 <<EOF
   cd GuestFacing/shippingconatiner_guest
   git pull
   docker-compose pull
