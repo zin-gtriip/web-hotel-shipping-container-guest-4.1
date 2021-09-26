8531e0036bad22ca000a68b9cb3ca2c7cddca13d
Phu Wai Paing <phuwai@gtriipglobal.com>
2021-09-26 20:52:47 +0800

    checkout bill format change and container connection change

--- guest_facing/check_out/static/check_out/js/desktop/bill.js
+++ guest_facing/check_out/static/check_out/js/desktop/bill.js
@@ -8,14 +8,14 @@ if ($('.modal[id*=form-error-modal]').length >= 1) {
 
 
 $('#id_reservation_no').change(function() {
-    window.location.href = window.location.origin +'/check_out/bill/'+ $(this).val();
+    window.location.href = window.location.origin +'/guest/check_out/bill/'+ $(this).val();
 });
 
 
 $('.modal[id*=message-modal-]').on('hide.bs.modal', function() {
     var complete = JSON.parse($('#check-out-complete').text() || '""');
     if (complete) {
-        window.location.href = '/check_out/complete/';
+        window.location.href = '/guest/check_out/complete/';
     }
 });
 
